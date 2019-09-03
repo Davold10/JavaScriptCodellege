5e8133e88797844675466e1a20e9dbf7e5bd16fa
David Romero <davidromero@Davids-MacBook-Pro.local>
2019-09-03 02:14:49 -0500

    Modista Se creo el HTML

---
+++ Secuenciales/Modista EJ6/app.js
@@ -0,0 +1,8 @@
+//CASHDOM
+let button = document.getElementById('button');
+let metros = document.getElementById('metros');
+let impresora = document.getElementById('result');
+
+//LISTENERS
+
+//FUNCTIONS
