1faa65172a1ee7f93d4702253ff1ecd0463dee55
David Romero <davidromero@Davids-MacBook-Pro.local>
2019-09-03 11:20:01 -0500

    JS

--- Secuenciales/Modista EJ6/app.js
+++ Secuenciales/Modista EJ6/app.js
@@ -2,7 +2,12 @@
 let button = document.getElementById('button');
 let metros = document.getElementById('metros');
 let impresora = document.getElementById('result');
+let total = 0;
 
 //LISTENERS
-
+button.addEventListener('click' , Pulgadas)
 //FUNCTIONS
+function Pulgadas(){
+    total =  metros.value * 39.3701;
+    impresora.innerHTML = total
+}
