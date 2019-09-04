0ea69eef6b8fd2b316e8bebde74c51cbd65bea11
David Romero <davidromero@Davids-MacBook-Pro.local>
2019-09-04 12:21:58 -0500

    JS

--- Condicionales simples/E2/app.js
+++ Condicionales simples/E2/app.js
@@ -4,14 +4,21 @@ let n1 = document.getElementById('n1');
 let n2 = document.getElementById('n2');
 let impresora = document.getElementById('result');
 //LISTENERS
+n1.addEventListener('blur', big)
+n2.addEventListener('blur', big)
 button.addEventListener('click', Comparar)
 //FUNCTIONS
+function big(){
+    if(isNaN(event.target.value)){
+        event.target.focus();
+    }
+}
 function Comparar() {
-    console.log('wtf');
-    
     if (n1.value > n2.value){
-        impresora.innerHTML = n1.value
+        impresora.innerHTML = n1.value;
+    } else if (n1.value < n2.value){
+        impresora.innerHTML = n2.value;
     } else {
-        impresora.innerHTML = n2.value
+        impresora.innerHTML = 'Los numeros tienen el mismo valor'
     }
 }
