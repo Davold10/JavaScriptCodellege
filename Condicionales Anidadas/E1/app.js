e8a83fb4ebe1592ed735dbd26a74e74ac4902153
David Romero <davidromero@Davids-MacBook-Pro.local>
2019-09-05 11:21:54 -0500

    JS 3 numeros terminado

---
+++ Condicionales Anidadas/E1/app.js
@@ -0,0 +1,28 @@
+//CASHDOM
+let button = document.getElementById('button');
+let N1 = document.getElementById('N1');
+let N2 = document.getElementById('N2');
+let N3 = document.getElementById('N3');
+let imp = document.getElementById('result');
+//LISTENERS
+button.addEventListener('click', Comparar);
+//FUNCTIONS
+function Comparar() {
+    if (N1.value > N2.value && N1.value > N3.value) {
+        imp.innerHTML = `
+        <h4 class="alert-heading text-info text-center"> El numero ${N1.value} es el mayor </h4>
+        `
+        return;
+    }
+    if (N2.value > N1.value && N2.value > N3.value) {
+        imp.innerHTML = `
+        <h4 class="alert-heading text-info text-center"> El numero ${N2.value} es el mayor </h4>
+        `
+        return;
+    }
+        imp.innerHTML = `
+        <h4 class="alert-heading text-info text-center"> El numero ${N3.value} es el mayor </h4>
+        `
+        return;
+
+}
