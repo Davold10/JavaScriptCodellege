2d2ba247730ffe6a95aa290f04ed6d39b397e8d2
David Romero <davidromero@Davids-MacBook-Pro.local>
2019-09-10 09:20:34 -0500

    HEHEHEHEH

---
+++ Estructuras Selectivas/Viajes P4/app.js
@@ -0,0 +1,11 @@
+//CASHDOM
+let budget = document.getElementById('budget');
+let result = document.getElementById('result')
+//LISTENERS
+budget.addEventListener('keypress', Destinations)
+//FUNCTIONS
+function Destinations() {
+    if(event.key == 'Enter'){
+        
+    }
+}
