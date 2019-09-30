5dab99632c2951b3a9c8d621fdebb5094245c3d5
David Romero <davidromero@Davids-MacBook-Pro.local>
2019-09-30 09:54:36 -0500

    JS E7-ACSII Falta terminar la logica.

---
+++ Arreglos/E7-ASCII/app.js
@@ -0,0 +1,15 @@
+//CASHDOM
+let InputWord = document.getElementById('InputWord');
+
+//LISTENERS
+InputWord.addEventListener('change', CodeASCII)
+
+//GLOBAL-VARIABLES
+let word = []
+let ASCIIm = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let ACSIM = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+//FUNCTIONS
+function CodeASCII() {
+
+}
